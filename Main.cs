1c6cb498d240182c61c0b74f44706da81d647037
Stanislav Raclavsky <stanislavraclavsky@gmail.com>
2022-10-27 20:47:39 +0200

    Version 1.0.0

---
+++ Main.cs
@@ -0,0 +1,72 @@
+﻿using UnityModManagerNet;
+using System;
+using System.Reflection;
+using System.Linq;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes;
+
+namespace RSDR
+{
+    internal class Main
+    {
+        internal static UnityModManagerNet.UnityModManager.ModEntry.ModLogger logger;
+        internal static Harmony.HarmonyInstance harmony;
+        internal static LibraryScriptableObject library;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                logger = modEntry.Logger;
+                harmony = Harmony.HarmonyInstance.Create(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                logger?.LogException(ex);
+                throw ex;
+            }
+            return true;
+        }
+        [Harmony.HarmonyPatch(typeof(LibraryScriptableObject), "LoadDictionary")]
+        [Harmony.HarmonyPatch(typeof(LibraryScriptableObject), "LoadDictionary", new Type[0])]
+        static class LibraryScriptableObject_LoadDictionary_Patch
+        {
+            [Harmony.HarmonyBefore(new string[] { "KingmakerAI" })]
+
+            static void Postfix(LibraryScriptableObject __instance)
+            {
+                var self = __instance;
+                if (Main.library != null) return;
+                Main.library = self;
+                try
+                {
+                    var AllBlueprints = Main.library.GetAllBlueprints();
+                    var SwarmDiminutiveFeature = AllBlueprints.First(x => x.name.Equals("SwarmDiminutiveFeature")) as BlueprintFeature;
+                    if (SwarmDiminutiveFeature)
+                    {
+                        Main.logger.Log("Removing SwarmDamageResistance from SwarmDiminutiveFeature...");
+                        var componentsWithoutDamageResistance = SwarmDiminutiveFeature.ComponentsArray.Where(delegate (BlueprintComponent c) {
+                            return c.GetType() != typeof(Kingmaker.UnitLogic.FactLogic.SwarmDamageResistance);
+                        }).ToArray();
+                        SwarmDiminutiveFeature.ComponentsArray = componentsWithoutDamageResistance;
+                    } else
+                    {
+                        Main.logger.Error("SwarmDiminutiveFeature blueprint not found!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Main.logger.LogException(ex);
+                }
+            }
+        }
+
+        internal static Exception Error(String message)
+        {
+            logger?.Log(message);
+            return new InvalidOperationException(message);
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Remove Swarm Damage Resistance
+
+A mod for Pathfinder: Kingmaker which removes damage resistance from all swarms.
+I programmed this mod primarily for myself because the inability to destroy the Spider Swarm almost put me off this game - maybe it will be useful for someone else.
+You can destroy the Spider Swarm and other swarms with your weapons now, enjoy :) 
+
+## Install
+- Build it using Visual Studio 2017 Community Edition or download zip archive from https://www.nexusmods.com/pathfinderkingmaker/mods/273
+- Drop archive to the Unity Mod Manager or copy RemoveSwarmDamageResistance.dll and info.json to the game mods folder, e.g. c:\EpicGames\PathfinderKingmaker\Mods\RemoveSwarmDamageResistance\
+- Install mod with the Unity Mod Manager
+- Run the game
+
+## Thanks
+- newman55 for creating the Unity Mod Manager
+- Jarinex for publishing Stolen land Encounters source code
